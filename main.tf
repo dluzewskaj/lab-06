4ded0c251e8d882ed9f011c20568a00d991948c8
dluzewskaj <117771781+dluzewskaj@users.noreply.github.com>
2026-01-22 11:51:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "{jenkins-state-julia}"   
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
 }
 resource "aws_s3_bucket" "example" {
-bucket = "jenkins-test-bucket-{yournamehere}"
+bucket = "jenkins-test-bucket-{julia5874281}"
 }
